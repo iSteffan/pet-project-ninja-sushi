3c566e4cdbb862bbf9ab349d0db250ed40738c3a
Stepan Skichko <st.skichko@gmail.com>
2023-03-14 17:42:50 +0200

    create sushi object

---
+++ src/js/sushi-object.js
@@ -0,0 +1,60 @@
+export const sushi = [
+  {
+    name: 'Гункан лосось',
+    price: 110,
+    weight: 40,
+    description: 'Лосось, норі, рис, ікра, соус шрірача, японський майонез',
+    image: '/src/images/sushi/gunkan-losos.jpg',
+  },
+  {
+    name: 'Гункан креветка',
+    price: 100,
+    weight: 40,
+    description: 'Норі, рис, ікра, креветка, соус шрірача, японський майонез',
+    image: '/src/images/sushi/gunkan-krevetka.jpg',
+  },
+  {
+    name: 'Гункан з тунцем і трюфелем',
+    price: 135,
+    weight: 40,
+    description:
+      'Норі, рис, японський майонез, бальзамик, трюфельна сальса, цибуля шніт',
+    image: '/src/images/sushi/gunkan-tunec-trufel.jpg',
+  },
+  {
+    name: 'Гункан тунець спайсі',
+    price: 125,
+    weight: 40,
+    description:
+      'Норі, рис, тунець, соус шрірача, японський майонез, кунжутна олія, цибуля шніт,',
+    image: '/src/images/sushi/gunkan-tunec-spicy.jpg',
+  },
+  {
+    name: 'Нігірі вугор',
+    price: 115,
+    weight: 40,
+    description: 'Норі, рис, кунжут, cоус унаги, вугор',
+    image: '/src/images/sushi/nigiri-ugor.jpg',
+  },
+  {
+    name: 'Нігірі вугор',
+    price: 190,
+    weight: 232,
+    description: 'Норі, рис, кунжут, cоус унаги, вугор',
+    image: '/src/images/sushi/nigiri-ugor2.jpg',
+  },
+  {
+    name: 'Гункан вугор',
+    price: 115,
+    weight: 40,
+    description: 'норі, рис, вугор, ікра, соус шрірача, японський майонез',
+    image: '/src/images/sushi/nigiri-ugor2.jpg',
+  },
+  {
+    name: 'Нігірі тунець',
+    price: 190,
+    weight: 40,
+    description: 'Рис, тунець',
+    image: '/src/images/sushi/nigiri-tunec.jpg',
+  },
+];
