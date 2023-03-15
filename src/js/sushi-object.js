a34705dd170aa7a527e14329e51e28178ee935f5
Stepan Skichko <st.skichko@gmail.com>
2023-03-15 16:34:19 +0200

    sushi cards IN PROCESS

--- src/js/sushi-object.js
+++ src/js/sushi-object.js
@@ -42,6 +42,8 @@ export const sushi = [
     description: 'Лосось, норі, рис, ікра, соус шрірача, японський майонез',
     image: gunkanLososURL,
     alt: 'Гункан лосось',
+    hit: 'Hit',
+    new: 'New',
   },
   {
     name: 'Гункан креветка',
@@ -50,6 +52,8 @@ export const sushi = [
     description: 'Норі, рис, ікра, креветка, соус шрірача, японський майонез',
     image: gunkanKrevetkaURL,
     alt: 'Гункан креветка',
+    hit: '',
+    new: 'New',
   },
   {
     name: 'Гункан з тунцем і трюфелем',
@@ -59,6 +63,8 @@ export const sushi = [
       'Норі, рис, японський майонез, бальзамик, трюфельна сальса, цибуля шніт',
     image: gunkanTunecTrufelURL,
     alt: 'Гункан з тунцем і трюфелем',
+    hit: '',
+    new: '',
   },
   {
     name: 'Гункан тунець спайсі',
@@ -68,6 +74,8 @@ export const sushi = [
       'Норі, рис, тунець, соус шрірача, японський майонез, кунжутна олія, цибуля шніт,',
     image: gunkanTunecSpicyURL,
     alt: 'Гункан тунець спайсі',
+    hit: '',
+    new: '',
   },
   {
     name: 'Нігірі вугор',
@@ -76,6 +84,8 @@ export const sushi = [
     description: 'Норі, рис, кунжут, cоус унаги, вугор',
     image: nigiriVygorURL,
     alt: 'Нігірі вугор',
+    hit: 'Hit',
+    new: '',
   },
   {
     name: 'Нігірі вугор',
@@ -84,6 +94,8 @@ export const sushi = [
     description: 'Норі, рис, кунжут, cоус унаги, вугор',
     image: nigiriVygor2URL,
     alt: 'Нігірі вугор',
+    hit: '',
+    new: '',
   },
   {
     name: 'Гункан вугор',
@@ -92,6 +104,8 @@ export const sushi = [
     description: 'норі, рис, вугор, ікра, соус шрірача, японський майонез',
     image: gunkanVygorURL,
     alt: 'Гункан вугор',
+    hit: '',
+    new: '',
   },
   {
     name: 'Нігірі тунець',
@@ -100,5 +114,7 @@ export const sushi = [
     description: 'Рис, тунець',
     image: nigiriTunecURL,
     alt: 'Нігірі тунець',
+    hit: '',
+    new: '',
   },
 ];
