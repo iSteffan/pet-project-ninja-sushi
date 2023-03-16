ec3de788f56e69c0d3a01f9e23b5a526ce5da38f
Stepan Skichko <st.skichko@gmail.com>
2023-03-16 17:15:26 +0200

    section sushi mobile DONE

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import { sushiMarkup } from './js/generate-sushi-cards';
 const refs = {
   sushiCardContainer: document.querySelector('.section-sushi__container'),
   loadSushiBtn: document.querySelector('.load-more-btn-sushi'),
+  productCard: document.querySelector('.product-card'),
 };
 
 // Вставляємо згенеровану розмітку карток суші в DOM
 refs.sushiCardContainer.insertAdjacentHTML('afterbegin', sushiMarkup);
+// console.log(refs.sushiCardContainer);
 
-refs.loadSushiBtn.addEventListener('click', onLoadMoreSushiClick);
+// refs.loadSushiBtn.addEventListener('click', onLoadMoreSushiClick);
 
-function onLoadMoreSushiClick(e) {
-  // const productCard = document.querySelector('.additional-info__new');
-  // // productCard.style.display = block;
-  // productCard.style.backgroundColor = 'teal';
-  // console.log(e);
-}
+// function onLoadMoreSushiClick(e) {
+//   // const productCard = document.querySelector('.additional-info__new');
+//   // // productCard.style.display = block;
+//   // productCard.style.backgroundColor = 'teal';
+//   console.log(productCard);
+// }
