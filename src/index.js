0f70174a57091d3332e28dae41d4691067e96301
Stepan Skichko <st.skichko@gmail.com>
2023-03-21 14:45:47 +0200

    js code refactoring
    повиносив у експорти функції з модалкою локації

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { sushiMarkup } from './js/generate-sushi-cards';
 import {
-  selectLanguage,
-  selectCity,
   onChangeLanguageClick,
   onChangeCityClick,
+  openModalLocation,
+  closeModalLocation,
 } from './js/modal-window-select-location';
 
 // import { hideSushiCard } from './js/load-more-sushi';
@@ -14,11 +14,8 @@ const refs = {
   productCards: document.querySelectorAll('.product-card'),
   btnOpen: document.querySelector('.location-btn'), // Відкриваємо модалку з вибором мови/міста
   btnClose: document.querySelector('.location-modal-window__close-btn'), // Закриваємо модалку з вибором мови/міста
-  modal: document.querySelector('[data-modal="location"]'), // Модалка з вибором мови/міста
   selectLanguage: document.querySelector('.location-modal__language'), // Кнопка з вибором мови
   selectCity: document.querySelector('.location-modal__city'), // Кнопка з вибором міста
-  setCity: document.querySelector('.location-btn__city'), // Встановлення міста
-  setLanguage: document.querySelector('.location-btn__language'), // Встановлення мови
 };
 
 // Вставляємо згенеровану розмітку карток суші в DOM
@@ -27,21 +24,6 @@ refs.sushiCardContainer.insertAdjacentHTML('afterbegin', sushiMarkup);
 // Відкриття/закриття модального вікна з вибором міста та мови
 refs.btnOpen.addEventListener('click', openModalLocation);
 refs.btnClose.addEventListener('click', closeModalLocation);
-
-function openModalLocation() {
-  refs.modal.classList.remove('is-hidden');
-}
-function closeModalLocation() {
-  refs.modal.classList.add('is-hidden');
-}
-
-//  Змінюємо місто і мову у хедері при натисканні на кнопки у модальному вікні
-refs.selectLanguage.addEventListener('click', onChangeLanguageClick);
-refs.selectCity.addEventListener('click', onChangeCityClick);
-
-//Змінюємо фон вибраної кнопки у модальному вікні
-selectLanguage();
-selectCity();
 
 const loadMoreButton = document.querySelector('.load-more-btn-sushi');
 const productCards = document.querySelectorAll('.product-card');
@@ -62,3 +44,7 @@ function loadMoreSushiClick() {
   }
   loadMoreButton.style.display = 'none';
 }
+
+document.addEventListener('DOMContentLoaded', firstStartHandle);
+
+function firstStartHandle() {}
--- src/js/modal-window-select-location.js
+++ src/js/modal-window-select-location.js
@@ -1,11 +1,14 @@
 const refs = {
+  modal: document.querySelector('[data-modal="location"]'), // Модалка з вибором мови/міста
   setCity: document.querySelector('.location-btn__city'), // Встановлення міста
   setLanguage: document.querySelector('.location-btn__language'), // Встановлення мови
   buttonsLanguage: document.querySelectorAll('.location-modal__language-btn'), // Масив кнопок з вибором мови
   buttonsCity: document.querySelectorAll('.location-modal__city-btn'), // Масив кнопок з вибором міста
+  selectLanguage: document.querySelector('.location-modal__language'), // Кнопка з вибором мови
+  selectCity: document.querySelector('.location-modal__city'), // Кнопка з вибором міста
 };
 
-export const selectLanguage = () => {
+const selectLanguage = () => {
   refs.buttonsLanguage.forEach(button => {
     button.addEventListener('click', () => {
       refs.buttonsLanguage.forEach(btn => btn.classList.remove('selected'));
@@ -14,7 +17,7 @@ export const selectLanguage = () => {
   });
 };
 
-export const selectCity = () => {
+const selectCity = () => {
   refs.buttonsCity.forEach(button => {
     button.addEventListener('click', () => {
       refs.buttonsCity.forEach(btn => btn.classList.remove('selected'));
@@ -23,16 +26,30 @@ export const selectCity = () => {
   });
 };
 
-export function onChangeLanguageClick(e) {
+function onChangeLanguageClick(e) {
   const language = e.target;
   if (language.nodeName === 'BUTTON') {
     refs.setLanguage.textContent = language.outerText;
   }
 }
 
-export function onChangeCityClick(e) {
+function onChangeCityClick(e) {
   const city = e.target;
   if (city.nodeName === 'BUTTON') {
     refs.setCity.textContent = city.outerText;
   }
+}
+
+export function openModalLocation() {
+  //  Змінюємо місто і мову у хедері при натисканні на кнопки у модальному вікні
+  refs.selectLanguage.addEventListener('click', onChangeLanguageClick);
+  refs.selectCity.addEventListener('click', onChangeCityClick);
+
+  selectLanguage(); //Змінюємо фон вибраної кнопки мови у модальному вікні
+  selectCity(); //Змінюємо фон вибраної кнопки міста у модальному вікні
+  refs.modal.classList.remove('is-hidden');
+}
+
+export function closeModalLocation() {
+  refs.modal.classList.add('is-hidden');
 }
