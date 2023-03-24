5eef79526721f96f22e0a47c672a7ab122588e25
Stepan Skichko <st.skichko@gmail.com>
2023-03-24 17:03:03 +0200

    add to cart btn DONE
    Потрібно отриманий масив по імені шукати в масиві об'єктів sushi. По співпадінню витягувати весь об'єкт, пушити в новий масив і цей масив закидувати в localStorage

--- src/index.js
+++ src/index.js
@@ -6,7 +6,41 @@ import { loadMoreSushi } from './js/load-more-sushi';
 const refs = {
   btnOpen: document.querySelector('.location-btn'), // Відкриваємо модалку з вибором мови/міста
   formSushiFilter: document.getElementById('sort-sushi'),
+  sushiContainer: document.querySelector('.section-sushi__container'),
+  addToCartBtn: document.querySelectorAll('.product-add-to-cart-btn'),
+  btnCartText: document.querySelectorAll('.btn-cart-text'),
 };
+
+let nameArr = [];
+
+refs.sushiContainer.addEventListener('click', e => {
+  const button = e.target.closest('.product-add-to-cart-btn');
+  if (button) {
+    const nameElement = button.closest('.product-card').querySelector('.product-name');
+    const addToCartText = button.closest('.product-card').querySelector('.btn-cart-text');
+    const addToCartIcon = button
+      .closest('.product-card')
+      .querySelector('.add-to-cart-icon');
+
+    const name = nameElement.textContent;
+    const index = nameArr.indexOf(name);
+
+    if (index === -1) {
+      nameArr.push(name);
+      button.style.backgroundColor = '#ccf5d5';
+      addToCartText.style.display = 'block';
+      addToCartIcon.style.display = 'none';
+    } else {
+      nameArr.splice(index, 1);
+      button.style.backgroundColor = '#f5f5f7';
+      addToCartText.style.display = 'none';
+      addToCartIcon.style.display = 'block';
+    }
+    localStorage.setItem('order', JSON.stringify(nameArr));
+    console.log('nameArr:', nameArr);
+    // console.log('arr:', nameArr);
+  }
+});
 
 // Події при завантаженні сторінки
 document.addEventListener('DOMContentLoaded', firstStartHandle);
