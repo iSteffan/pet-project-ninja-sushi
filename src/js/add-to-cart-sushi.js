5188425098c25283a44d205570c8405e69152050
Stepan Skichko <st.skichko@gmail.com>
2023-03-25 16:10:02 +0200

    add sushi by click to cart DONE

--- src/js/add-to-cart-sushi.js
+++ src/js/add-to-cart-sushi.js
@@ -40,6 +40,7 @@ export function addToCartArr(e) {
 }
 
 function addDomCartMarkup() {
+  refs.cartContainer.innerHTML = '';
   const addCartMarkup = addItemToCart();
   refs.cartContainer.insertAdjacentHTML('afterbegin', addCartMarkup);
 }
@@ -51,24 +52,35 @@ function addItemToCart() {
 
   const cartMarkup = parsedOrder
     .map(item => {
-      return `<div>
-  <img src="${item.image}" alt="${item.alt}" />
-  <div>
-    <h2>${item.name}</h2>
-    <p>Вага: ${item.weight} г</p>
-  </div>
-  <div>
-    <p>${item.price} грн</p>
-    <button type="button">-</button>
-    <p>1</p>
-    <button type="button">+</button>
-  </div>
-  <button type="button">
-    <svg width="30" height="30">
-      <use href="./images/symbol-defs.svg#menu-close"></use>
-    </svg>
-  </button>
-</div>`;
+      return `<div class="cart-item">
+                <div class="cart-item__description">
+                    <img src="${item.image}" alt="${item.alt}" class="cart-item__image" />
+                    <div class="">
+                        <h2 class="cart-item__name">${item.name}</h2>
+                        <p class="cart-item__weight">Вага: ${item.weight} г</p>
+                    </div>
+                </div>
+                <div class="cart-item__price-wrapper">
+                    <p class="cart-item__price">${item.price} грн</p>
+                    <button type="button" class="decrease-amount">
+                        <svg width="18" height="18" viewBox="0 0 32 32">
+                            <path d="M5.333 16c0-0.736 0.597-1.333 1.333-1.333h18.667c0.736 0 1.333 0.597 1.333 1.333s-0.597 1.333-1.333 1.333h-18.667c-0.736 0-1.333-0.597-1.333-1.333z"></path>
+                        </svg>
+                    </button>
+                    <p class="item-count">1</p>
+                    <button type="button" class="increase-amount">
+                        <svg width="18" height="18" viewBox="0 0 32 32">
+                            <path d="M16 5.333c0.736 0 1.333 0.597 1.333 1.333v18.667c0 0.736-0.597 1.333-1.333 1.333s-1.333-0.597-1.333-1.333v-18.667c0-0.736 0.597-1.333 1.333-1.333z"></path>
+                            <path d="M5.333 16c0-0.736 0.597-1.333 1.333-1.333h18.667c0.736 0 1.333 0.597 1.333 1.333s-0.597 1.333-1.333 1.333h-18.667c-0.736 0-1.333-0.597-1.333-1.333z"></path>
+                        </svg>
+                    </button>
+                </div>
+                <button type="button" class="delete-from-cart-btn">
+                    <svg width="16" height="16" viewBox="0 0 32 32">
+                        <path d="M23.733 10.304 22.229 8.8l-5.963 5.963L10.303 8.8l-1.504 1.504 5.963 5.963-5.963 5.963 1.504 1.504 5.963-5.963 5.963 5.963 1.504-1.504-5.963-5.963 5.963-5.963z"/>
+                    </svg>
+                </button>
+            </div>`;
     })
     .join('');
 
