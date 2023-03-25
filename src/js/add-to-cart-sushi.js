99ac647b20262dccc2319e50fc30170514245e5c
Stepan Skichko <st.skichko@gmail.com>
2023-03-25 12:02:38 +0200

    bug fixed

--- src/js/add-to-cart-sushi.js
+++ src/js/add-to-cart-sushi.js
@@ -35,43 +35,42 @@ export function addToCartArr(e) {
 
     localStorage.setItem('order', JSON.stringify(orderSushi));
 
-    // const addCartMarkup = addItemToCart();
-    // console.log(addItemToCart());
+    addDomCartMarkup();
   }
 }
 
-// export function addDomCartMarkup() {
-//   const addCartMarkup = addItemToCart();
-//   refs.cartContainer.insertAdjacentHTML('afterbegin', addCartMarkup);
-// }
+function addDomCartMarkup() {
+  const addCartMarkup = addItemToCart();
+  refs.cartContainer.insertAdjacentHTML('afterbegin', addCartMarkup);
+}
 
-// function addItemToCart() {
-//   const savedOrder = localStorage.getItem('order');
-//   const parsedOrder = JSON.parse(savedOrder);
-//   console.log(parsedOrder);
+function addItemToCart() {
+  const savedOrder = localStorage.getItem('order');
+  const parsedOrder = JSON.parse(savedOrder);
+  //   console.log(parsedOrder);
 
-//   const cartMarkup = parsedOrder
-//     .map(item => {
-//       return `<div>
-//   <img src="${item.image}" alt="${item.alt}" />
-//   <div>
-//     <h2>${item.name}</h2>
-//     <p>Вага: ${item.weight} г</p>
-//   </div>
-//   <div>
-//     <p>${item.price} грн</p>
-//     <button type="button">-</button>
-//     <p>1</p>
-//     <button type="button">+</button>
-//   </div>
-//   <button type="button">
-//     <svg width="30" height="30">
-//       <use href="./images/symbol-defs.svg#menu-close"></use>
-//     </svg>
-//   </button>
-// </div>`;
-//     })
-//     .join('');
+  const cartMarkup = parsedOrder
+    .map(item => {
+      return `<div>
+  <img src="${item.image}" alt="${item.alt}" />
+  <div>
+    <h2>${item.name}</h2>
+    <p>Вага: ${item.weight} г</p>
+  </div>
+  <div>
+    <p>${item.price} грн</p>
+    <button type="button">-</button>
+    <p>1</p>
+    <button type="button">+</button>
+  </div>
+  <button type="button">
+    <svg width="30" height="30">
+      <use href="./images/symbol-defs.svg#menu-close"></use>
+    </svg>
+  </button>
+</div>`;
+    })
+    .join('');
 
-//   return cartMarkup;
-// }
+  return cartMarkup;
+}
