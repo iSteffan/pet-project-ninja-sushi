015751fc24edd9be38193d328424a3ee1a23f9f1
Stepan Skichko <st.skichko@gmail.com>
2023-03-28 10:19:46 +0300

    mobile menu shape DONE

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ loadMoreSushi();
   closeMenuBtn.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  window.matchMedia('(min-width: 1575px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
