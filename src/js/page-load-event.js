7a917a63faf9c5afd5579040f529996340785ad2
Stepan Skichko <st.skichko@gmail.com>
2023-04-12 12:47:49 +0300

    improved meditation
    модалка не показується при повторному заході на сторінку

--- src/js/page-load-event.js
+++ src/js/page-load-event.js
@@ -5,18 +5,31 @@ const refs = {
 
 // Функція для перевірки часу та виведення модального вікна про медитацію
 export function checkMeditationTime() {
+  //Зчитуємо дані зі сховища
+  const visited = localStorage.getItem('visited');
+  const parsedVisited = JSON.parse(visited);
+
   const currentHour = new Date().getHours();
 
   // Перевіряємо, чи знаходиться час в проміжку між 12:00 і 13:59 та нічого не робимо
   if (currentHour >= 12 && currentHour <= 13) {
     return;
   }
-  // Інакше:
-  // Показуємо модальне вікно
-  refs.meditationModal.classList.remove('hide');
 
-  // Додаємо слухача на кнопку
-  refs.meditationBtnClose.addEventListener('click', meditationCloseHandler);
+  // Інакше:
+  // Виконуємо код нижче тільки, якщо це перший захід на сторінку та у сховищі немає ключа 'visited'
+  if (!parsedVisited) {
+    // Створюємо ключ, таким чином, якщо користувач вже відвідував сторінку то при наступному перезавантаженні
+    // він не побачить модальне вікно
+    let isVisited = true;
+    localStorage.setItem('visited', JSON.stringify(isVisited));
+
+    // Показуємо модальне вікно
+    refs.meditationModal.classList.remove('hide');
+
+    // Додаємо слухача на кнопку
+    refs.meditationBtnClose.addEventListener('click', meditationCloseHandler);
+  }
 }
 
 function meditationCloseHandler() {
